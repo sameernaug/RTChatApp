da16d5f8b78a5ba7b30ad3ecb003181dd6f7f7b4
Sameer Naugariya <0603sameernaug@gmail.com>
2025-06-21 20:16:15 +0530

    Fixed error

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "/";
+const BASE_URL = "https://rtchatapp-f7lv.onrender.com/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
