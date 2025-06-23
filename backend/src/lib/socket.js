390567b1cd3273a3f2552ff074764b19ce8ea061
Sameer Naugariya <0603sameernaug@gmail.com>
2025-06-24 04:02:35 +0530

    final commit

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === "production"
       ? "https://rtchatapp.netlify.app"
-      : "http://localhost:5173",
+      : ["http://localhost:5173"],
     credentials: true,
   },
 });
