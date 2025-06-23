a1771256c390ece674ed030a3d5930a5d071411e
Sameer Naugariya <0603sameernaug@gmail.com>
2025-06-24 03:52:51 +0530

    fixed cors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://rtchatapp-f7lv.onrender.com/api",
+  // baseURL: "http://localhost:8080/api",
   withCredentials: true,
 });
